e49db655cf0713bd8ecdf0ed9e41c802c21de68b
nooby6 <giftaudrine1@gmail.com>
2024-11-20 17:19:33 +0300

    Add SQLAlchemy model for User to define users table structure

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Defines a SQLAlchemy model named User for a database table named users.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """SQLAlchemy model for the users table."""
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
