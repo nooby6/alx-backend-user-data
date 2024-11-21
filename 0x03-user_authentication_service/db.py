a47d1f9b8511d9c242a7740bb41c23469769d4c6
nooby6 <giftaudrine1@gmail.com>
2024-11-21 21:44:26 +0300

    Add DB module for user authentication service with CRUD operations

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""DB module"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine(
+            "sqlite:///a.db",
+        )
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Saves a user to the database
+
+        Return:
+            the saved User
+        """
+        if email and hashed_password:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Returns the first user filtered on the keyword arguments"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
+
+        if not user:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user's attributes as per kwargs"""
+        user = self.find_user_by(id=user_id)
+        for attr, val in kwargs.items():
+            if attr in User.__table__.columns:
+                setattr(user, attr, val)
+            else:
+                raise ValueError
+        self._session.commit()
